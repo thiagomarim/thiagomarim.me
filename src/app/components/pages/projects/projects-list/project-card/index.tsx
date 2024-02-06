5f49ad864837b29a03b4c3b51c9d9a16cdab52fb
Thiago <thiago.marim2005@gmail.com>
2024-02-06 13:47:11 -0300

    Adicionado modal de ver fotos do projetos

--- src/app/components/pages/projects/projects-list/project-card/index.tsx
+++ src/app/components/pages/projects/projects-list/project-card/index.tsx
@@ -2,6 +2,8 @@ import { Project } from "@/app/types/project";
 import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
 import { TiArrowForward } from "react-icons/ti";
+import * as Dialog from "@radix-ui/react-dialog";
+import { IoMdClose } from "react-icons/io";
 
 interface ProjectCardProps {
   project: Project;
@@ -10,27 +12,46 @@ interface ProjectCardProps {
 export function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div className="flex flex-col rounded-lg h-[500px] bg-[#202024] border-2 border-[#323238] group">
-      <div className="w-full h-48 overflow-hidden">
-        <Image
-          src={project.thumbnail.url}
-          alt={`Foto do projeto ${project.title}`}
-          width={380}
-          height={200}
-          unoptimized
-          className="w-full rounded-md h-full object-cover group-hover:scale-110 transition-all group-hover:duration-500 opacity-70 group-hover:opacity-100 px-8 pt-8"
-        />
-      </div>
-
+      <Dialog.Root>
+        <div className="w-full h-48 overflow-hidden">
+          <Dialog.Trigger>
+            <Image
+              src={project.thumbnail.url}
+              alt={`Foto do projeto ${project.title}`}
+              width={380}
+              height={200}
+              unoptimized
+              className="w-full rounded-sm h-full object-cover group-hover:scale-110 transition-all group-hover:duration-500 opacity-70 group-hover:opacity-100 hover:cursor-zoom-in px-8 pt-8"
+            />
+          </Dialog.Trigger>
+        </div>
+        <Dialog.Portal>
+          <Dialog.Overlay className="inset-0 fixed bg-black/60" />
+          <Dialog.Content className="z-10 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Close className="absolute right-0 top-0 p-1.5 bg-[#121214]">
+              <IoMdClose size={20} />
+            </Dialog.Close>
+            <Image
+              src={project.thumbnail.url}
+              alt={`Thumbnail do projeto ${project.title}`}
+              width={700}
+              height={400}
+              className="outline-none"
+              quality={100}
+            />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
       <div className="flex flex-col flex-1 p-8">
         <strong className="text-[#E1E1E6] text-xl">{project.title}</strong>
-        <div className="text-[#A9A9B2] text-base mt-2 h-[120px]">
+        <div className="text-[#A9A9B2] text-base mt-2 mb-4 h-[120px] flex-1">
           {project.shortDescription}
         </div>
         <div className="flex items-center flex-wrap gap-2">
           {project.technologies.map((tech) => (
             <span
               key={tech.name}
-              className="max-w-max text-[#E1E1E6] bg-blue-500 text-sm py-1 px-3 rounded-lg"
+              className="max-w-max text-[#E1E1E6] bg-blue-500 text-sm py-1 px-2 rounded-md"
             >
               {tech.name}
             </span>
